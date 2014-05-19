1d48c90e1e7b79e5ea1aa07c0a59e5074eea790f
musary <churui.ctec@gmail.com>
2014-05-19 09:00:40 +0800

    v0.3
    1. add number of page comparison function. If page number in
    content.txt is different from it in BDSD file, program exit
    automatically.

--- mergeMorePDF.py
+++ mergeMorePDF.py
@@ -1,4 +1,4 @@
-import re,os,operator
+import re,os,operator,sys
 from PyPDF2 import PdfFileReader, PdfFileMerger, utils
 
 runningDt = os.getcwd()
@@ -14,8 +14,9 @@ testPages = []
 testTableFullName = []
 testTableName = []
 testTableDict = {}
+totalInTxt = 0
 
-#
+#Pattern1 - RPC-AT & ESF-AT Test Table
 #RE patern for test table file name like 'R0-032 1_2.1 LL PZR Press SI.pdf'
 #token: _2.1  we can use m.group(2) as test item number.
 reTableItem1_1='.*?'	# Non-greedy match on filler
@@ -24,7 +25,7 @@ reTableItem1_3='([+-]?\\d*\\.\\d+)(?![-+0-9\\.])'	# Float 1
 reTableItem1_4='( )'	# Any Single Character 2
 rgTableItem1 = re.compile(reTableItem1_1+reTableItem1_2+reTableItem1_3+reTableItem1_4,re.IGNORECASE|re.DOTALL)
 
-#
+#Pattern2 - RPC-AT Test Table
 #RE patern for test table file name like 'R0-032 1_2.1.1 LL PZR Press SI.pdf'
 #token: _2.1.1  we can use m.group(2+3+4) as test item number.
 reTableItem2_1='.*?'	# Non-greedy match on filler
@@ -35,7 +36,7 @@ reTableItem2_5='([+-]?\\d*\\.\\d+)(?![-+0-9\\.])'	# Float 1
 reTableItem2_6='( )'	# White Space 1
 rgTableItem2 = re.compile(reTableItem2_1+reTableItem2_2+reTableItem2_3+reTableItem2_4+reTableItem2_5+reTableItem2_6,re.IGNORECASE|re.DOTALL)
 
-#
+#Pattern3 - RPC-AT Test Table
 #RE partern for test table file name like 'R0-032 1_2.1-D LL PZR Press SI.pdf'
 #token: _2.1-D, we will use group(2~6) for test item name
 reTableItem3_1='.*?'	# Non-greedy match on filler
@@ -48,7 +49,7 @@ reTableItem3_6='(-)'	# Any Single Character 3
 reTableItem3_7='(D)'	# Any Single Character 4
 rgTableItem3 = re.compile(reTableItem3_1+reTableItem3_2+reTableItem3_3+reTableItem3_4+reTableItem3_5+reTableItem3_6+reTableItem3_7,re.IGNORECASE|re.DOTALL)
 
-#
+#Pattern4 - RPC-AT Test Table
 #RE partern for test table file name like 'R0-032 1_2.1.1-D LL PZR Press SI.pdf'
 #token: _2.1.1-D, we could use group(2~8) for test item name
 reTableItem4_1='.*?'	# Non-greedy match on filler
@@ -61,6 +62,28 @@ reTableItem4_7='(\\d+)'	# Integer Number 3
 reTableItem4_8='(-)'	# Any Single Character 4
 reTableItem4_9='(D)'	# Any Single Character 5
 rgTableItem4 = re.compile(reTableItem4_1+reTableItem4_2+reTableItem4_3+reTableItem4_4+reTableItem4_5+reTableItem4_6+reTableItem4_7+reTableItem4_8+reTableItem4_9,re.IGNORECASE|re.DOTALL)
+
+#Pattern5 - ESF-AT
+#RE for test table file name like 'R1-01 A_1 C1 to RGL (RPCC).pdf'
+#token: '_1 '. use group(2) for test item number.
+reTableItem5_1='.*?'	# Non-greTableItem5_edy match on filler
+reTableItem5_2='(_)'	# Any Single Character 1
+reTableItem5_3='(\\d+)'	# Integer Number 1
+reTableItem5_4='(\\s+)'	# White Space 1
+
+rgTableItem5 = re.compile(reTableItem5_1+reTableItem5_2+reTableItem5_3+reTableItem5_4,re.IGNORECASE|re.DOTALL)
+
+#Pattern6 - ESF-AT
+#RE for test table file name like 'R2-02 A_3+4 Auxiliary Feedwater Signal.pdf'
+#token: '_3+4 '. use group(2,3,4) for test item number
+reTableItem6_1='.*?'	# Non-greedy match on filler
+reTableItem6_2='(_)'	# Any Single Character 1
+reTableItem6_3='(\\d+)'	# Integer Number 1
+reTableItem6_4='(\\+)'	# Any Single Character 2
+reTableItem6_5='(\\d+)'	# Integer Number 2
+reTableItem6_6='(\\s+)'	# White Space 1
+
+rgTableItem6 = re.compile(reTableItem6_1+reTableItem6_2+reTableItem6_3+reTableItem6_4+reTableItem6_5+reTableItem6_6,re.IGNORECASE|re.DOTALL)
 
 #
 #RE patern for BDSD page name like:'1.12-D  -  1/7'
@@ -90,6 +113,7 @@ def dictIncrement(dictThing,currentPage,pageIncrement):
             dictThing[key] = value + pageIncrement
     return dictThing 
 
+#extract test item number from test table PDf files.
 def testItemPattern(BDSDPage):
     testItem = ''
     if rgBDSDFirstPageNum.search(BDSDPage) != None:
@@ -106,7 +130,9 @@ def testItemPattern(BDSDPage):
     
     return testItem
 
+#manipulate content.txt
 def BDSDContentFillter(fname):
+    global totalInTxt
     testItems = []
     testPages = []
     with open(fname) as f:
@@ -123,6 +149,11 @@ def BDSDContentFillter(fname):
             testPages.append(page)
 
     testItemsPages = dict(zip(testItems,testPages))
+    #print testItemsPages
+    f = file('test items.txt','w')
+    f.writelines(testItemsPages)
+    f.close()
+    totalInTxt =  page 
     return testItemsPages
 
 #
@@ -175,6 +206,12 @@ def getTestItem(testTableName):
     elif rgTableItem4.search(testTableName) != None:
         m = rgTableItem4.search(testTableName)
         testItem = m.group(2)+m.group(3)+m.group(4)+m.group(5)+m.group(6)+m.group(7)+m.group(8)
+    elif rgTableItem5.search(testTableName) != None:
+        m = rgTableItem5.search(testTableName)
+        testItem = m.group(2)
+    elif rgTableItem6.search(testTableName) != None:
+        m = rgTableItem6.search(testTableName)
+        testItem = m.group(2)+m.group(3)+m.group(4)
     else:
         print "Error:\nfile name '%s' is wrong, please check it!" % testTableName
 
@@ -183,7 +220,7 @@ def getTestItem(testTableName):
 #Manipulate PDF files. Insert test table into logic diagram by page number.
 #
 def manipulatePDF():
-    global BDSDFullName,contentTXT
+    global BDSDFullName,contentTXT,totalInTxt
     global testTableName, testTableFullName,testTableDict
     input0 = PdfFileReader(file(BDSDFullName[0],'rb'))
     merger1 = PdfFileMerger()
@@ -193,6 +230,11 @@ def manipulatePDF():
     i=0 #count how many test tables are inserted to BDSD file.
     tableCount = 0
     testItemsPagesInitial = BDSDContentFillter(contentTXT)
+    if numBDSD != totalInTxt+1:
+        print '''\nError!\nNumber of pages in "content.txt" are different from the "BDSD file".
+This process is forced to stop. Please check both files and then start over again.'''
+        raw_input('Press any key to quit...')
+        sys.exit(0)
     exceptCount = False
     for testTable in testTableDict:
         try:
@@ -220,7 +262,8 @@ def manipulatePDF():
     if exceptCount:
         print "Warning: output PDF file couldn't be used in case there is an Error.\n"
     else:
-        print "%d Test Tables successfully merged to %s, please check the output file." % tableCount, BDSDFullName[0]
+        print "\n%d Test Tables successfully merged to \"%s\", please check the output file." % (tableCount, BDSDFullName[0])
+    #print testItemsPagesInitial
 
     
 walkTableDirectory(tableDt)
